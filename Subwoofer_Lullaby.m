1866fbde8e888fad2329b440cd3ca3693621f472
TheSockCollector <lsoliver@calpoly.edu>
2025-11-10 13:54:17 -0800

    BEAUTIFUL

--- Subwoofer_Lullaby.m
+++ Subwoofer_Lullaby.m
@@ -1,76 +1,159 @@
 %% Subwoofer Lulluby
 
-midi(79, 0.25);
-pause(0.25);
+%% notes
+count = 0.875;
+half = (count/2);
 
-%2 seconds (on MIDI chart)
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+b5 = midi(83, count);
+c5 = midi(72, count);
+e5 = midi(76, count);
+g5 = midi(79, count);
+c4 = midi(60, count);
+b4 = midi(71, count);
+e6 = midi(88, count);
+g6 = midi(91, count);
+a6 = midi(93, count);
 
-midi(72, 0.25);
-pause(0.25);
+b5s = midi(83, half);
+c6s = midi(84, half);
+d6s = midi(86, half);
+e6s = midi(88, half);
 
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+fs = 44100;
+bits = 24;
 
-midi(79, 0.25);
-pause(0.25);
+%% song
+sound(c5, fs, bits);
+pause(count)
 
-%3 seconds
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+pause(count);
 
-midi(72, 0.25);
-pause(0.25);
+sound(g5, fs, bits)
+pause(count);
 
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+%start of 4th bar (on MIDI chart)
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+pause(count);
 
-midi(79, 0.25);
-pause(0.25);
+sound(c5, fs, bits);
+pause(count)
 
-%4 seconds
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+pause(count);
 
-midi(72, 0.25);
-pause(0.25);
+sound(g5, fs, bits)
+pause(count);
 
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+%5th bar
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+pause(count);
 
-midi(79, 0.25);
-pause(0.25);
+sound(c5, fs, bits);
+sound(c4, fs, bits);
+pause(count);
 
-%5 seconds
-midi(76, 0.25);
-midi(83, 0.25);
-pause(0.25);
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(c5, fs, bits);
+pause(count);
 
-midi(72, 0.25);
-pause(0.25);
+sound(c4, fs, bits)
+sound(g5, fs, bits)
+pause(count);
 
-midi(76, 0.25);
-midi(83, 0.25);
-midi(72, 0.25);
-pause(0.25)
+%6th bar
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(c5, fs, bits);
+pause(count);
 
-midi(79, 0.25);
-pause(0.25);
+sound(c5, fs, bits);
+sound(c4, fs, bits);
+pause(count);
 
-%6 seconds
-midi(76, 0.25);
-midi(83, 0.25);
-midi(72, 0.25);
-pause(0.25)
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(b4, fs, bits);
+pause(count);
+
+sound(c4, fs, bits)
+sound(g5, fs, bits)
+pause(count);
+
+%7th bar
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(b4, fs, bits);
+pause(count);
+
+sound(c5, fs, bits);
+sound(c4, fs, bits);
+pause(count);
+
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(c5, fs, bits);
+pause(count);
+
+sound(c4, fs, bits)
+sound(g5, fs, bits)
+pause(count);
+
+%8th bar
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(c5, fs, bits);
+pause(count);
+
+sound(c5, fs, bits);
+sound(c4, fs, bits);
+pause(count);
+
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(b4, fs, bits);
+pause(count);
+
+sound(c4, fs, bits)
+sound(g5, fs, bits)
+pause(count);
+
+%% 9th bar
+sound(e5, fs, bits)
+sound(b5, fs, bits)
+sound(b4, fs, bits)
+pause(count);
+
+sound(b5s, fs, bits)
+sound(c5, fs, bits)
+pause(half)
+
+sound(c6s, fs, bits)
+pause(half)
+
+sound(d6s, fs, bits)
+sound(e5, fs, bits)
+sound(b4, fs, bits)
+pause(half)
+
+sound(e6, fs, bits)
+pause(half)
+
+sound(g4, fs, bits)
+pause(half)
+
+sound(d6, fs, bits)
+pause(half)
+
+%10th bar
 
 
-%haven't tested this yet, will in a couple hours when I get out of class.
+
+
 % I'm using https://onlinesequencer.net/1187399 and https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.phys.unsw.edu.au%2Fjw%2Fgraphics%2Fnotes.GIF&f=1&nofb=1&ipt=31d5228003e6dcd2c39359fb15ee3350ba1863989583ae9891423135d3fb4913
 % ^^ for the mapping and conversion.
--- midi.m
+++ midi.m
@@ -2,4 +2,4 @@ function waveform = midi(number, duration)
 frequency = (440*2^((number-69)/12));
 
 
-waveform = myCos(frequency, 0, duration, 41400);
+waveform = myCos(frequency, 0, duration, 44100) + 0.6*myCos(frequency/2, 0, duration, 44100) + 0.0*myCos(frequency/3, 0, duration, 44100)+0.3*myCos(frequency/4, 0, duration, 44100);
