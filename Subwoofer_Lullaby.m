4a6a06cbcc4f6a489c7e79debbf098f059837132
TheSockCollector <lsoliver@calpoly.edu>
2025-11-10 09:58:05 -0800

    instructions

--- Subwoofer_Lullaby.m
+++ Subwoofer_Lullaby.m
@@ -9,3 +9,5 @@ midi(83, 0.25);
 pause(0.25)
 
 %haven't tested this yet, will in a couple hours when I get out of class.
+% I'm using https://onlinesequencer.net/1187399 and https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.phys.unsw.edu.au%2Fjw%2Fgraphics%2Fnotes.GIF&f=1&nofb=1&ipt=31d5228003e6dcd2c39359fb15ee3350ba1863989583ae9891423135d3fb4913
+% ^^ for the mapping and conversion.
